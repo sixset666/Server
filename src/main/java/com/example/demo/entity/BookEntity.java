33ed8a35b9b624b3c66c5c21bed5df2ed1f8d670
sixset666 <mamonov9583@mail.ru>
2023-03-07 11:25:51 +0300

    3 commit

--- src/main/java/com/example/demo/entity/BookEntity.java
+++ src/main/java/com/example/demo/entity/BookEntity.java
@@ -4,6 +4,9 @@ package com.example.demo.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 
 @Getter
 @Setter
@@ -18,13 +21,16 @@ public class BookEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String title;
-    private String author;
-    private String publishing;
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private AuthorEntity author;
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "publishing_id")
+    private PublisherEntity publishing;
     private int year;
     private String kind;
-
-
-
-
 }
--- src/main/java/com/example/demo/entity/PublisherEntity.java
+++ src/main/java/com/example/demo/entity/PublisherEntity.java
@@ -1,8 +1,11 @@
 package com.example.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 @Entity
 @Data
@@ -11,4 +14,10 @@ public class PublisherEntity {
    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+   @NotBlank
+    private String publisher;
+   private String city;
+   @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "publishing")
+    private List<BookEntity> book;
 }
